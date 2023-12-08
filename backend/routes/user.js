df4c69b157a0085eff23814c415e36dc911e5133
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2023-12-08 15:13:47 +0100

    Added validation upon creating new users in backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const { create } = require('../controllers/user');
+const express = require("express");
+
+const { create } = require("../controllers/user");
+const { userValidator, validate } = require("../middlewares/validator");
 
 const router = express.Router();
 
-router.post('/create', create);
+router.post("/create", userValidator, validate, create);
 
 module.exports = router;
